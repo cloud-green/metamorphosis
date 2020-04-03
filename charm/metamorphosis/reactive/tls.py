b3b4aa0bb64673700a7afdc0f4b6dead7dbe8dd6
Ales Stimec <ales.stimec@canonical.com>
2020-04-03 15:14:52 +0200

    TLS cleanup on departing easyrsa relation.

--- charm/metamorphosis/reactive/tls.py
+++ charm/metamorphosis/reactive/tls.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-from charmhelpers.core import hookenv
+from charmhelpers.core import hookenv, unitdata
 
 from charms.reactive import (when, when_file_changed, remove_state,
                              when_not, set_state)
@@ -10,6 +10,7 @@ from charms.layer import tls_client
 from charms.layer.metamorphosis import (METAMORPHOSIS_CA_CERT,
                                         METAMORPHOSIS_CERT,
                                         METAMORPHOSIS_KEY)
+from charms.reactive.helpers import data_changed
 
 
 @when('certificates.available')
@@ -45,3 +46,49 @@ def import_ca_crt_to_keystore():
 def assertDirExists(path):
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path), exist_ok=True)
+
+
+@when('endpoint.certificates.departed')
+def clear_certificates():
+    if os.path.exists(METAMORPHOSIS_CA_CERT):
+        os.remove(METAMORPHOSIS_CA_CERT)
+
+    certs_paths = unitdata.kv().get('layer.tls-client.cert-paths', {})
+    if os.path.exists(METAMORPHOSIS_CERT):
+        os.remove(METAMORPHOSIS_CERT)
+    if os.path.exists(METAMORPHOSIS_KEY):
+        os.remove(METAMORPHOSIS_KEY)
+
+    for common_name, paths in certs_paths.get('client', {}).items():
+        data_changed(
+            'layer.tls-client.client.{}'.format(
+                common_name
+            ),
+            {}
+        )
+
+    # We need to clean up data because the underlying layer
+    # failes to clear flags and data_changed states when
+    # we remove {charm}-easyrsa relation.
+    unitdata_keys = (
+        'reactive.tls_client',
+        'reactive.data_changed.endpoint.certificates',
+        'reactive.data_changed.ca_certificate',
+        'reactive.data_changed.certificate',
+        'reactive.data_changed.client',
+        'reactive.data_changed.server',
+        'reactive.states.endpoint.certificates',
+        'reactive.states.tls_client'
+        'reactive.data_changed.layer.tls-client'
+    )
+    for k in unitdata_keys:
+        unitdata.kv().unsetrange(None, k)
+
+    cleanup_states = (
+        'tls_client.ca.saved',
+        'tls_client.server.certificate.saved',
+        'tls_client.server.key.saved',
+        'metamorphosis.ca.certificate.saved'
+    )
+    for s in cleanup_states:
+        remove_state(s)
